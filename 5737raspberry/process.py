201604a896f25c1161bd1adca7db2754630eab0b
Mark Li <mark.haoxiang@gmail.com>
2019-01-24 20:44:43 +0800

    Detection

---
+++ 5737raspberry/process.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy
+import math
+from enum import Enum
+from gripball import GripDetectBall
+
+#5737 python code running on raspberry pi coprocessor
+cam = cv2.VideoCapture(0) #init cameras
+
+data = {}
+
+while True:
+    _,img = cam.read()
+    ball = GripDetectBall()
+    ball.process(img)
+    ballOutput = ball.find_blobs_output
+    if ballOutput != []:
+        c = 0
+        maximumSize = 0
+        for i in range(len(ballOutput)):
+            if ballOutput[i].size > maximumSize:
+                c = i
+                maximumSize = ballOutput[i].size
+        key = ballOutput[c]
+        data["ballPtX"] = key.pt[0]]
+        data["ballSize"] = key.size
+        time.sleep(.1)
+    
